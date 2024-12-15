82f1780909264cc5adaef8f63b8c4a0e37e2fc8c
AymanDoukali <aym99699@gmail.com>
2024-12-15 18:50:02 +0100

    - OOP code structure - Separate config.yaml file - logging setup

--- testing/test_m.py
+++ testing/test_m.py
@@ -1,64 +1,63 @@
-import data as dt
-import vm_control as vm
+from os.path import abspath
+import yaml
 import subprocess
 import time
+import logging.config
+from VirtualMachine import VirtualMachine, VMwareVM
+
+# Load the YAML file
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+# Apply the logging configuration
+logging.config.dictConfig(config)
+logger = logging.getLogger('test_logger')
 
 
-def test_m_vm(m: str, soft: str, vm_name: str, detail: bool=True) -> int:
+def test_m_vm(m: str, vm: VirtualMachine) -> int:
     """
-    test_m_vm runs the method on a virtual machine
+    test_m_vm runs the method m on the virtual machine vm
     :param m: method to test
-    :param soft: virtualization software
-    :param vm_name: virtual machine name
-    :param detail: detailed output
+    :param vm: virtual machine to test
     :return: 0 if physical machine, 1 if virtual machine
     """
-    print(f"Testing method {m} on virtual machine {vm_name} ({soft})\n")
+    print(f"Testing method {m} on virtual machine {vm.name} ({vm.soft})\n")
+    logger.info(f"Testing method {m} on virtual machine {vm.name} ({vm.soft})\n")
     try:
         # Starting the virtual machine
-        if detail:
-            print("Starting virtual machine...")
-        vm.start_vm(vm_name, soft)
+        logger.info("Starting virtual machine...")
+        vm.start()
         # Waiting for the virtual machine to completely starts
-        time.sleep(30)
+        time.sleep(config['consts']['DELAY'])
         # Copying exe to the vm
-        if detail:
-            print("Copying executable to virtual machine...")
-        host_path = f"{dt.EXE_FULL_PATH}{m}.exe"
-        guest_path = f"{dt.GUEST_PATH}{m}.exe"
-        vm.copy_to_vm(vm_name, soft, host_path, guest_path)
+        logger.info("Copying executable to virtual machine...")
+        EXE_FULL_PATH = abspath(config['paths']['EXE_PATH']) + "\\"
+        host_path = f"{EXE_FULL_PATH}{m}.exe"
+        guest_path = f"C:\\Users\\{config['vm'][vm.soft][vm.name]['user']}\\Desktop\\{m}.exe"
+        vm.copy_to_vm(host_path, guest_path)
         # Running the exe on the vm
-        if detail:
-            print("Running executable on virtual machine...")
-        output = vm.run_on_vm(vm_name, soft, guest_path)
-        if detail:
-            print(output.stdout)
+        logger.info("Running executable on virtual machine...")
+        output = vm.run_on_vm(guest_path)
+        logger.debug(output.stdout)
         # Saving the vm state
-        if detail:
-            print("Saving virtual machine state...")
-        vm.savestate_vm(vm_name, soft)
+        logger.info("Saving virtual machine state...\n")
+        vm.save_state()
         return output.returncode
     except Exception as e:
-        print(e)
+        logger.error(e)
 
 
-# print(test_m_vm("m1", "vmware", "Win8", detail=True))
-
-
-def test_m_pm(m:str, detail:bool) -> int:
+def test_m_pm(m:str) -> int:
     """
     test_m_pm runs the method on the current machine
     :param m: method to test
-    :param detail: detailed output
     :return: 0 if physical machine, 1 if virtual machine
     """
+    logger.info(f"Testing method {m} on this machine:\n")
     print(f"Testing method {m} on this machine:\n")
     try:
-        output = subprocess.run([dt.EXE_PATH + m + ".exe"], capture_output=True, text=True)
-        if detail:
-            print(output.stdout)
+        output = subprocess.run([config['paths']["EXE_PATH"] + m + ".exe"], capture_output=True, text=True)
+        logger.debug(output.stdout)
         return output.returncode
     except Exception as e:
-        print(e)
-
-# print(test_m_pm("m1", False))
+        logger.error(e)
